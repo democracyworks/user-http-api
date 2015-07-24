8b544259e6cc9c44db3a24e6c7c0fb72622740b0
Wes Morgan <cap10morgan@gmail.com>
2015-07-24 13:27:46 -0600

    [WM] remove unneeded start-event-handler! fn
    HTTP API gateways shouldn't be getting any incoming RabbitMQ messages. They just relay incoming web requests and the responses.

--- src/user_http_api/channels.clj
+++ src/user_http_api/channels.clj
@@ -6,30 +6,6 @@
 (defonce update-users (async/chan))
 (defonce delete-users (async/chan))
 
-(defn start-event-handler!
-  "Start a new thread listening for messages on `channel` and passing
-  them to `handler`. Will loop over all messages, logging errors. When
-  `channel` is closed, stop looping. Or the loop can be stopped by
-  calling the `:stop!` function returned."
-  [channel handler]
-  (let [stop-chan (async/chan)
-        signal-chan (async/thread
-                      (loop []
-                        (async/alt!!
-                          stop-chan :stopped
-                          channel ([event]
-                                   (if (nil? event)
-                                     :closed
-                                     (do
-                                      (try
-                                        (handler event)
-                                        (catch Throwable t
-                                          (println t "Error handling event."
-                                                   (pr-str event))))
-                                      (recur)))))))]
-    {:stop! (fn [] (async/put! stop-chan :stop))
-     :notes signal-chan}))
-
 (defn close-all! []
   (doseq [c [create-users read-users update-users delete-users]]
     (async/close! c)))
